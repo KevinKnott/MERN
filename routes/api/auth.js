af42393b6f9045b4e392c0d354b92e4649f77b83
Kevin Knott <KKnoTT93@gmail.com>
2020-02-19 18:38:58 -0600

    Backend authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/auth
-// @desc    Test route
+// @route   POST api/auth
+// @desc    Authenticate user and get token
 // @access  Public
 router.get('/', auth, async (req, res) => {
     try {
@@ -15,5 +19,68 @@ router.get('/', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+router.post(
+    '/',
+    [
+        check('email', 'Please include a valid email')
+            .isEmail(),
+        check('password', 'Please enter a password'
+        ).exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (!user) {
+                return res.status(400).json({ msg: "Invalid Credentials" })
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res
+                    .status(400)
+                    .json({ msg: "Invalid Credentials" })
+            }
+
+            // Encrypt password with bcrypt
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            // Return a JWT
+
+            const payload = {
+                user: {
+                    // Not underscored because mongoose abstracts this out
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error')
+        }
+    }
+)
 
 module.exports = router;
